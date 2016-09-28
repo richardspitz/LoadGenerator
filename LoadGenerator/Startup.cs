009ca80b936fd764ab4acbe98557be043dd7dbe9
NillsF <nills.franssens@gmail.com>
2016-09-28 10:37:37 +0200

    Add project files.

---
+++ LoadGenerator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoadGenerator.Startup))]
+namespace LoadGenerator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
